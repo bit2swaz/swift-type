1a51ef59d279cabe7070d564384bdfbea0737d73
Aditya <bit2swaz@gmail.com>
2025-05-24 20:00:46 +0000

    update script.js

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ let currentTestWords = [];
 let currentWordIndex = 0;
 let currentCharIndex = 0;
 
+let testActive = false;
+let testFinished = false;
+let timerId = null;
+let timeLeft = 60;
+let initialTestTime = 60;
