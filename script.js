0bfdcd0d12b997926e89a18a3ecc69c9a9c19bb1
Aditya <bit2swaz@gmail.com>
2025-05-24 20:01:04 +0000

    update script.js

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ let testFinished = false;
 let timerId = null;
 let timeLeft = 60;
 let initialTestTime = 60;
+
+let correctChars = 0;
+let incorrectChars = 0;
+let totalTypedChars = 0;
+let typedCorrectlyOnce = 0;
+let startTime = 0;
+
