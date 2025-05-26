0840ea1728ef8e12310aa0a1cb653f88d327acd3
Aditya <bit2swaz@gmail.com>
2025-05-26 09:09:03 +0000

    added DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+// --- DOM Elements ---
+const wordsDisplay = document.getElementById('words-display');
+const textInput = document.getElementById('text-input');
+const timerDisplay = document.getElementById('timer');
+const wpmDisplay = document.getElementById('wpm');
+const accuracyDisplay = document.getElementById('accuracy');
+const restartBtn = document.getElementById('restart-btn');
